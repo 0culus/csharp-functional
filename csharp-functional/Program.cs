12fdeb8ce7722694f8719c3685c2bdf1e253a5c7
Sean Turner <sturner@nmt.edu>
2015-07-22 02:35:59 -0600

    Move some stuff to the outer class scope

--- csharp-functional/Program.cs
+++ csharp-functional/Program.cs
@@ -10,19 +10,26 @@ namespace csharp_functional
 {
     class Program
     {
+        public static Option<int> RawInput1 { get; set; } = Some(123);
+        public static Option<int> RawInput2 { get; set; } = Some(123);
+        public static int UserIntInput1 { get; set; }
+        public static int UserIntInput2 { get; set; }
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("C# can be pretend to be functional now! Press ESC to quit");
             do
             {
                 Console.Write("Enter an integer: ");
-                var rawInput = Some(123);
-                var _userInput = rawInput.Match(
+                
+                UserIntInput1 = RawInput1.Match(
                     Some: v => v * 2,
                     None: () => 0
                     );
 
-                if (int.TryParse(Console.ReadLine(), out _userInput))
+                var userIntInput1 = UserIntInput1;
+                if (int.TryParse(Console.ReadLine(), out userIntInput1))
                 {
                     Console.WriteLine("One down...");
                 }
